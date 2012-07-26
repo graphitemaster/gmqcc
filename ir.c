b4225ea98ae6a3fd1178035cffaedae308c2981c
Wolfgang Bumiller <wolfgang.linux@bumiller.com>
2012-07-26 22:11:46 +0200

    INSTR_NOT_* output to opC not opB...

--- ir.c
+++ ir.c
@@ -2287,10 +2287,8 @@ tailcall:
             stmt.o1.u1 = stmt.o3.u1;
             stmt.o3.u1 = 0;
         }
-        else if ((stmt.opcode >= INSTR_STORE_F    &&
-                  stmt.opcode <= INSTR_STORE_FNC)    ||
-                 (stmt.opcode >= INSTR_NOT_F      &&
-                  stmt.opcode <= INSTR_NOT_FNC))
+        else if (stmt.opcode >= INSTR_STORE_F &&
+                 stmt.opcode <= INSTR_STORE_FNC)
         {
             /* 2-operand instructions with A -> B */
             stmt.o2.u1 = stmt.o3.u1;
