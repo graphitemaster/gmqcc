36591e902ba0c9f37404ff75f3f83b2cad0ad2ae
Wolfgang (Blub) Bumiller <blub@speed.at>
2012-08-14 12:17:34 +0200

    it's .file, not .name'

--- error.c
+++ error.c
@@ -58,7 +58,7 @@ void printmsg(int level, const char *name, size_t line, const char *msgtype, con
 
 void cvprintmsg(lex_ctx ctx, int lvl, const char *msgtype, const char *msg, va_list ap)
 {
-    vprintmsg(lvl, ctx.name, ctx.line, msgtype, msg, ap);
+    vprintmsg(lvl, ctx.file, ctx.line, msgtype, msg, ap);
 }
 
 void cprintmsg (lex_ctx ctx, int lvl, const char *msgtype, const char *msg, ...)
