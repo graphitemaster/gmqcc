9ed9a0c669cde318174bfbabf9db43d8795ca773
Wolfgang (Blub) Bumiller <blub@speed.at>
2012-08-14 22:34:20 +0200

    get rid of more duplicate code

--- parser.c
+++ parser.c
@@ -431,13 +431,9 @@ static bool parser_sy_pop(parser_t *parser, shunt *sy)
             return true;
 
         case opid1('+'):
-            if (exprs[0]->expression.vtype != exprs[1]->expression.vtype) {
-                parseerror(parser, "invalid types used in expression: cannot add type %s and %s",
-                           type_name[exprs[0]->expression.vtype],
-                           type_name[exprs[1]->expression.vtype]);
-                return false;
-            }
-            if (exprs[0]->expression.vtype != TYPE_VECTOR && exprs[0]->expression.vtype != TYPE_FLOAT) {
+            if (exprs[0]->expression.vtype != exprs[1]->expression.vtype ||
+                (exprs[0]->expression.vtype != TYPE_VECTOR && exprs[0]->expression.vtype != TYPE_FLOAT) )
+            {
                 parseerror(parser, "invalid types used in expression: cannot add type %s and %s",
                            type_name[exprs[0]->expression.vtype],
                            type_name[exprs[1]->expression.vtype]);
@@ -458,16 +454,12 @@ static bool parser_sy_pop(parser_t *parser, shunt *sy)
             };
             break;
         case opid1('-'):
-            if (exprs[0]->expression.vtype != exprs[1]->expression.vtype) {
+            if (exprs[0]->expression.vtype != exprs[1]->expression.vtype ||
+                (exprs[0]->expression.vtype != TYPE_VECTOR && exprs[0]->expression.vtype != TYPE_FLOAT) )
+            {
                 parseerror(parser, "invalid types used in expression: cannot subtract type %s from %s",
                            type_name[exprs[1]->expression.vtype],
                            type_name[exprs[0]->expression.vtype]);
-                return false;
-            }
-            if (exprs[0]->expression.vtype != TYPE_VECTOR && exprs[0]->expression.vtype != TYPE_FLOAT) {
-                parseerror(parser, "invalid types used in expression: cannot subtract type %s from %s",
-                           type_name[exprs[0]->expression.vtype],
-                           type_name[exprs[1]->expression.vtype]);
                 return false;
             }
             switch (exprs[0]->expression.vtype) {
