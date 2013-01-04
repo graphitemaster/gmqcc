ec4cd0d25825dae640904254bacbc3b1e5366c72
Dale Weiler <killfieldengine@gmail.com>
2013-01-04 12:26:44 +0000

    Disable enhanced diagnostics as they're too slow.

--- opts.c
+++ opts.c
@@ -65,7 +65,7 @@ static void opts_setdefault() {
     opts_set(opts.flags, FTEPP_PREDEFS,                  false);
     opts_set(opts.flags, CORRECT_TERNARY,                true);
     opts_set(opts.flags, BAIL_ON_WERROR,                 true);
-    opts_set(opts.flags, ENHANCED_DIAGNOSTICS,           true);
+    opts_set(opts.flags, ENHANCED_DIAGNOSTICS,           false);
 }
 
 void opts_init(const char *output, int standard, size_t arraysize) {
