1e60bafcdc825e1ec3a847ca6b3b5f43599afe29
Dale Weiler <killfieldengine@gmail.com>
2013-02-06 09:28:42 +0000

    Aliases to aliases is a bad idea, revert.  Added better tests to show unused aliases

--- parser.c
+++ parser.c
@@ -5280,13 +5280,6 @@ static bool parse_variable(parser_t *parser, ast_block *localblock, bool nofield
                         void           *entry = (void*)var->desc;
                         ast_expression *find  = parser_find_var(parser, var->desc);
 
-                        /* aliases to aliases are also allowed */
-                        if (!find) {
-                            char *name = NULL;
-                            if ((find = parser_find_var(parser, (const char *)(name = util_htget(parser->aliases, var->desc)))))
-                                entry = (void*)name;
-                        }
-
                         if (!find) {
                             compile_error(parser_ctx(parser), "undeclared variable `%s` for alias `%s`", var->desc, var->name);
                             return false;
