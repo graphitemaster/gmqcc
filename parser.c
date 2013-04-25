35692c0b579825cd73e030af73e7f8718c52559b
Wolfgang Bumiller <wry.git@bumiller.com>
2013-04-25 19:18:11 +0200

    Revert "Just fucking initialize everything"
    This reverts commit aed2b1031c6466a2dbcf909faeac1ac1cff0f6e6.

--- parser.c
+++ parser.c
@@ -5639,11 +5639,11 @@ skipvar:
         }
 
         if (parser->tok == '#') {
-            ast_function *func        = NULL;
-            ast_value    *number      = NULL;
-            float         fractional  = 0;
-            float         integral    = 0;
-            int           builtin_num = 0;
+            ast_function *func   = NULL;
+            ast_value    *number = NULL;
+            float         fractional;
+            float         integral;
+            int           builtin_num;
 
             if (localblock) {
                 parseerror(parser, "cannot declare builtins within functions");
