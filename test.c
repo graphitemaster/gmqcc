d411d6068524252056039f543fcebcad480a5b24
Dale Weiler <killfieldengine@gmail.com>
2013-07-21 22:07:22 +0000

    Eeasy fix for that

--- test.c
+++ test.c
@@ -707,7 +707,7 @@ static bool task_propagate(const char *curdir, size_t *pad, const char *defs) {
              * so we don't trample over an existing one.
              */
             tmpl->tempfilename = NULL;
-            util_asprintf(&tmpl->tempfilename, "%s/TMPDAT.%s", curdir, files->d_name);
+            util_asprintf(&tmpl->tempfilename, "%s/TMPDAT.%s.dat", curdir, files->d_name);
 
             /*
              * Additional QCFLAGS enviroment variable may be used
